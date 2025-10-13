fe0014fac62d445409a0cabd8a782a5c82efa7b3
Hillary Tanui <hillarytanui80@gmail.com>
2025-10-13 22:30:24 +0300

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handlePassChange =() => console.log('Entering password...');
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handlePassChange}></input>
+        </div>
     )
 }
 
